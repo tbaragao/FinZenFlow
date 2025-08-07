d50bc0532d023c7a634e9068d2ac00b5fe734afb
Thiago Aragão <tbaragao@gmail.com>
2025-08-07 00:55:29 -0300

    feat: implements authentication using clerk

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import {
+	clerkMiddleware,
+	ClerkMiddlewareAuth,
+	createRouteMatcher,
+} from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware(
+	async (auth: ClerkMiddlewareAuth, req: NextRequest) => {
+		console.log(req);
+		if (isProtectedRoute(req)) await auth.protect();
+
+		return NextResponse.next();
+	}
+);
+
+export const config = {
+	matcher: [
+		// Skip Next.js internals and all static files, unless found in search params
+		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		// Always run for API routes
+		"/(api|trpc)(.*)",
+	],
+};
